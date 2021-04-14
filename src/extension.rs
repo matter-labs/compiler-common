322654bed031656247ed26e6f2594daee1e7e739
Alex Z <a.zarudnyy@matterlabs.dev>
2021-04-14 13:21:27 +0300

    Add the LLVM and Solidity file extensions

--- src/extension.rs
+++ src/extension.rs
@@ -5,14 +5,20 @@
 /// The manifest file extension.
 pub static MANIFEST: &str = "toml";
 
+/// The JSON data file extension.
+pub static JSON: &str = "json";
+
 /// The Zinc source code file extension.
 pub static ZINC: &str = "zn";
 
 /// The Yul source code file extension.
 pub static YUL: &str = "yul";
 
+/// The Solidity source code file extension.
+pub static SOLIDITY: &str = "sol";
+
+/// The LLVM source code file extension.
+pub static LLVM: &str = "ll";
+
 /// The assembly file extension.
 pub static ASSEMBLY: &str = "zasm";
-
-/// The JSON data file extension.
-pub static JSON: &str = "json";

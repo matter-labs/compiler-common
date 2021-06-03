b6c31484e5a50bf0e60fff6fb9bd38aa601c9c6c
hedgar2017 <hedgar2017@gmail.com>
2021-06-03 20:32:08 +0300

    Add contract context field names, separate output file names

--- src/file_name.rs
+++ src/file_name.rs
@@ -14,5 +14,11 @@ pub static LIBRARY_ENTRY: &str = "lib";
 /// The source code complex module default name.
 pub static MODULE_ENTRY: &str = "mod";
 
-/// The assembly file default name.
-pub static ASSEMBLY: &str = "main";
+/// The LLVM IR output file default name.
+pub static LLVM_IR: &str = "main";
+
+/// The zkEVM assembly output file default name.
+pub static ZKEVM_ASSEMBLY: &str = "main";
+
+/// The zkEVM binary output file default name.
+pub static ZKEVM_BINARY: &str = "main";
--- src/identifier.rs
+++ src/identifier.rs
@@ -8,8 +8,17 @@ pub static FUNCTION_MAIN: &str = crate::file_name::APPLICATION_ENTRY;
 /// The contract entry selector function name.
 pub static FUNCTION_SELECTOR: &str = "__selector";
 
-/// The implicit `msg` variable name.
+/// The intrinsic `msg` variable name.
 pub static VARIABLE_MSG: &str = "msg";
 
-/// The implicit `block` variable name.
+/// The intrinsic `msg.sender` field name.
+pub static FIELD_MSG_SENDER: &str = "sender";
+
+/// The intrinsic `block` variable name.
 pub static VARIABLE_BLOCK: &str = "block";
+
+/// The intrinsic `block.number` field name.
+pub static FIELD_BLOCK_NUMBER: &str = "number";
+
+/// The intrinsic `block.timestamp` field name.
+pub static FIELD_BLOCK_TIMESTAMP: &str = "timestamp";

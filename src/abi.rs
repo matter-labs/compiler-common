e78043f73a55578cdabbde3b81115fcb74c8e915
Alex Z <a.zarudnyy@matterlabs.dev>
2021-08-15 14:49:08 +0300

    Add some ABI constants

--- src/abi.rs
+++ src/abi.rs
@@ -13,3 +13,12 @@ pub const OFFSET_ENTRY_HASH: usize = 7;
 
 /// The call and return data.
 pub const OFFSET_CALL_RETURN_DATA: usize = 8;
+
+/// The constructor entry hash.
+pub const CONSTRUCTOR_ENTRY_HASH: u32 = 0;
+
+/// The constructor flag key preimage.
+pub static CONSTRUCTOR_EXECUTED_FLAG_KEY_PREIMAGE: &str = "zkSyncEVM_ConstructorExecuted";
+
+/// The Solidity memory representation zero slot offset.
+pub const OFFSET_SOLIDITY_ZERO_SLOT: usize = 3;

211382f43f231cfa51add5218167c6a1cb402202
Alex Z <a.zarudnyy@matterlabs.dev>
2021-08-26 14:48:15 +0300

    Change the constructor bit layout

--- src/abi.rs
+++ src/abi.rs
@@ -8,14 +8,14 @@ pub const OFFSET_CALLDATA_SIZE: usize = 0;
 /// The return data size.
 pub const OFFSET_RETURN_DATA_SIZE: usize = 1;
 
-/// The entry function signature `keccak256` hash.
-pub const OFFSET_ENTRY_HASH: usize = 7;
+/// The entry data, e.g. the method hash and constructor bit.
+pub const OFFSET_ENTRY_DATA: usize = 7;
 
 /// The call and return data.
 pub const OFFSET_CALL_RETURN_DATA: usize = 8;
 
-/// The constructor entry hash.
-pub const CONSTRUCTOR_ENTRY_HASH: u32 = 0;
+/// The constructor entry mask.
+pub const CONSTRUCTOR_ENTRY_MASK: u32 = 1;
 
 /// The constructor flag key preimage.
 pub static CONSTRUCTOR_EXECUTED_FLAG_KEY_PREIMAGE: &str = "zkSyncEVM_ConstructorExecuted";

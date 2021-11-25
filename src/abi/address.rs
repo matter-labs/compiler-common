46e182d590b97a441e86eeea47f640830c37f17a
Alex Z <a.zarudnyy@matterlabs.dev>
2021-11-25 13:38:05 +0200

    Pad ETH addresses with zeros

--- src/abi/address.rs
+++ src/abi/address.rs
@@ -3,10 +3,10 @@
 //!
 
 /// The `keccak256` predefined address.
-pub static KECCAK256: &str = "0x10";
+pub static KECCAK256: &str = "0x0000000000000000000000000000000000000010";
 
 /// The `sha256` predefined address.
-pub static SHA256: &str = "0x11";
+pub static SHA256: &str = "0x0000000000000000000000000000000000000011";
 
 /// The `ecrecover` predefined address.
-pub static ECRECOVER: &str = "0x12";
+pub static ECRECOVER: &str = "0x0000000000000000000000000000000000000012";
--- src/solidity/address.rs
+++ src/solidity/address.rs
@@ -3,10 +3,10 @@
 //!
 
 /// The `ecrecover` predefined address.
-pub static ECRECOVER: &str = "0x01";
+pub static ECRECOVER: &str = "0x0000000000000000000000000000000000000001";
 
 /// The `sha256` predefined address.
-pub static SHA256: &str = "0x02";
+pub static SHA256: &str = "0x0000000000000000000000000000000000000002";
 
 /// The `ripemd160` predefined address.
-pub static RIPEMD160: &str = "0x03";
+pub static RIPEMD160: &str = "0x0000000000000000000000000000000000000003";

f98253708c079a6835774397e4115b69c1c4809d
Alex Z <a.zarudnyy@matterlabs.dev>
2021-12-08 17:08:02 +0200

    Add the deployed contracts counter

--- src/abi/storage.rs
+++ src/abi/storage.rs
@@ -4,3 +4,6 @@
 
 /// The constructor flag key preimage.
 pub static IS_CONSTRUCTOR_EXECUTED: &str = "zkSyncEVM_ConstructorExecuted";
+
+/// The deployed contracts counter.
+pub static DEPLOYED_CONTRACTS_COUNTER: &str = "zkSyncEVM_DeployedContractsCounter";

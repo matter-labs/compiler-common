6568cbda685b224a10cf809329f0d1c1f13bfeb2
Alex Z <a.zarudnyy@matterlabs.dev>
2022-04-25 22:18:50 +0200

    Add the return data offsets

--- src/abi/memory.rs
+++ src/abi/memory.rs
@@ -5,11 +5,14 @@
 /// The heap size in cells.
 pub const SIZE: usize = 0x80000;
 
-/// The calldata and return data offset's offset (cells).
-pub const OFFSET_DATA_OFFSET: usize = SIZE - 1;
+/// The calldata offset's offset (cells).
+pub const OFFSET_CALLDATA_OFFSET: usize = SIZE - 1;
 
-/// The calldata and return data length's offset (cells).
-pub const OFFSET_DATA_LENGTH: usize = SIZE - 2;
+/// The calldata length's offset (cells).
+pub const OFFSET_CALLDATA_LENGTH: usize = SIZE - 2;
 
-/// The long-return flag offset (cells).
-pub const OFFSET_LONG_RETURN: usize = SIZE - 3;
+/// The return data offset's offset (cells).
+pub const OFFSET_RETURN_DATA_OFFSET: usize = SIZE - 3;
+
+/// The return data length's offset (cells).
+pub const OFFSET_RETURN_DATA_LENGTH: usize = SIZE - 4;

673e5205d1b464d9c106bd8990ec59833769a6fb
Alex Z <a.zarudnyy@matterlabs.dev>
2022-05-11 20:52:46 +0200

    Move the zkSync contract to 0x8000+X

--- src/abi/address.rs
+++ src/abi/address.rs
@@ -3,28 +3,22 @@
 //!
 
 /// The bootloader formal address.
-pub static BOOTLOADER: &str = "0x0000000000000000000000000000000000000001";
+pub static BOOTLOADER: &str = "0x0000000000000000000000000000000000008001";
 
 /// The contract deployer system contract address.
-pub static CONTRACT_DEPLOYER: &str = "0x0000000000000000000000000000000000000002";
+pub static CONTRACT_DEPLOYER: &str = "0x0000000000000000000000000000000000008002";
 
 /// The nonce holder system contract address.
-pub static NONCE_HOLDER: &str = "0x0000000000000000000000000000000000000003";
+pub static NONCE_HOLDER: &str = "0x0000000000000000000000000000000000008003";
 
 /// The known code factory system contract address.
-pub static KNOWN_CODES_STORAGE: &str = "0x0000000000000000000000000000000000000004";
+pub static KNOWN_CODES_STORAGE: &str = "0x0000000000000000000000000000000000008004";
 
 /// The immutable simulator system contract address.
-pub static IMMUTABLE_SIMULATOR: &str = "0x0000000000000000000000000000000000000005";
+pub static IMMUTABLE_SIMULATOR: &str = "0x0000000000000000000000000000000000008005";
 
 /// The `keccak256` predefined address.
-pub static KECCAK256: &str = "0x0000000000000000000000000000000000000010";
-
-/// The `sha256` predefined address.
-pub static SHA256: &str = "0x0000000000000000000000000000000000000011";
-
-/// The `ecrecover` predefined address.
-pub static ECRECOVER: &str = "0x0000000000000000000000000000000000000012";
+pub static KECCAK256: &str = "0x0000000000000000000000000000000000008010";
 
 /// The `to_l1` simulation predefined address.
 pub static TO_L1: &str = "0x000000000000000000000000000000000000FFFF";

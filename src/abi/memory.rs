66a24163226d5a3fdc47b978b4e2cf89ac026a10
Alex Z <a.zarudnyy@matterlabs.dev>
2022-05-30 16:04:15 +0200

    Add the temporary msg.value ABI data preserving cells

--- src/abi/memory.rs
+++ src/abi/memory.rs
@@ -17,6 +17,12 @@ pub const OFFSET_RETURN_DATA_OFFSET: usize = SIZE - 3;
 /// The return data length's offset (cells).
 pub const OFFSET_RETURN_DATA_LENGTH: usize = SIZE - 4;
 
+/// The temporary storage of the data replaced by `msg.value` of the ABI calldata (cells).
+pub const OFFSET_ABI_VALUE_TEMPORARY_DATA: usize = SIZE - 5;
+
+/// The temporary storage of the data replaced by `address` of the ABI calldata (cells).
+pub const OFFSET_ABI_ADDRESS_TEMPORARY_DATA: usize = SIZE - 6;
+
 /// The Vyper forwarder calldata space offset (cells).
 pub const OFFSET_VYPER_FORWARDER_CALLDATA: usize = SIZE - 64;
 
